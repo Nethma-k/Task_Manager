6e18a32e9654fda4897deb0c2dbc26e04a1cd447
Nethma-k <111905294+Nethma-k@users.noreply.github.com>
2023-09-25 13:19:11 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 require('dotenv').config();
 
-const MONGO_URL= 'mongodb+srv://nka:nka123@cluster0.adm6m4a.mongodb.net/';
+const MONGO_URL= 'mongodb+srv://nka:aVFFVWiuSi9DuxR8@cluster0.adm6m4a.mongodb.net/';
 const DB_NAME = 'db1';
 
 mongoose.connect(MONGO_URL,{

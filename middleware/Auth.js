3e9245babb0f64aa5c419bf5a09bd0267a0cdf60
Nethma-k <111905294+Nethma-k@users.noreply.github.com>
2023-09-25 11:21:44 +0530

    Update Auth.js

--- middleware/Auth.js
+++ middleware/Auth.js
@@ -1,10 +1,11 @@
 const jwt = require ('jsonwebtoken');
 const User = require('../models/User');
+const jwtSecretKey = 'nka';
 
 const auth = async (req,res,next) =>{
     try{
         const token = req.header('Authorization').replace('Bearer ', ' ');
-        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+        const decoded = jwt.verify(token, jwtSecretKey );
         const user = await User.findOne({
             _id : decoded._id,
         })
